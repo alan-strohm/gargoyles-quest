47d6ebca6577adbbac0536e5dff35aadfcaf3fef
Alan Strohm <3605394+alan-strohm@users.noreply.github.com>
2025-05-17 15:43:42 -0700

    factor out player class

---
+++ src/game/player/Player.ts
@@ -0,0 +1,122 @@
+import { Scene } from "phaser";
+
+export class Player {
+  private sprite: Phaser.Physics.Arcade.Sprite;
+  private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+  private speed: number = 175;
+
+  constructor(scene: Scene, x: number, y: number) {
+    // Create the player sprite
+    this.sprite = scene.physics.add
+      .sprite(x, y, "atlas", "misa-front")
+      .setSize(30, 40)
+      .setOffset(0, 24);
+
+    // Create the player's walking animations
+    const anims = scene.anims;
+    anims.create({
+      key: "misa-left-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-left-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    anims.create({
+      key: "misa-right-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-right-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    anims.create({
+      key: "misa-front-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-front-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    anims.create({
+      key: "misa-back-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "misa-back-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    // Get cursor keys
+    const keyboard = scene.input.keyboard;
+    if (!keyboard) {
+      throw new Error("Keyboard input not available");
+    }
+    this.cursors = keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (!this.sprite.body) {
+      return;
+    }
+
+    const body = this.sprite.body as Phaser.Physics.Arcade.Body;
+    const prevVelocity = body.velocity.clone();
+
+    // Stop any previous movement from the last frame
+    body.setVelocity(0);
+
+    // Horizontal movement
+    if (this.cursors.left.isDown) {
+      body.setVelocityX(-this.speed);
+    } else if (this.cursors.right.isDown) {
+      body.setVelocityX(this.speed);
+    }
+
+    // Vertical movement
+    if (this.cursors.up.isDown) {
+      body.setVelocityY(-this.speed);
+    } else if (this.cursors.down.isDown) {
+      body.setVelocityY(this.speed);
+    }
+
+    // Normalize and scale the velocity so that player can't move faster along a diagonal
+    body.velocity.normalize().scale(this.speed);
+
+    // Update the animation last and give left/right animations precedence over up/down animations
+    if (this.cursors.left.isDown) {
+      this.sprite.anims.play("misa-left-walk", true);
+    } else if (this.cursors.right.isDown) {
+      this.sprite.anims.play("misa-right-walk", true);
+    } else if (this.cursors.up.isDown) {
+      this.sprite.anims.play("misa-back-walk", true);
+    } else if (this.cursors.down.isDown) {
+      this.sprite.anims.play("misa-front-walk", true);
+    } else {
+      this.sprite.anims.stop();
+
+      // If we were moving, pick and idle frame to use
+      if (prevVelocity.x < 0) this.sprite.setTexture("atlas", "misa-left");
+      else if (prevVelocity.x > 0) this.sprite.setTexture("atlas", "misa-right");
+      else if (prevVelocity.y < 0) this.sprite.setTexture("atlas", "misa-back");
+      else if (prevVelocity.y > 0) this.sprite.setTexture("atlas", "misa-front");
+    }
+  }
+
+  // Getters for collision and camera setup
+  getSprite(): Phaser.Physics.Arcade.Sprite {
+    return this.sprite;
+  }
+} 
--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,8 +1,9 @@
 import { Scene } from "phaser";
+import { Player } from "../player/Player";
 
 export class Game extends Scene {
-  player: Phaser.Physics.Arcade.Body;
-  cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+  private player: Player;
+  private worldLayer: Phaser.Tilemaps.TilemapLayer | null = null;
 
   constructor() {
     super("Game");
@@ -14,12 +15,20 @@ export class Game extends Scene {
     // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
     // Phaser's cache (i.e. the name you used in preload)
     const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+    if (!tileset) {
+      throw new Error("Failed to load tileset");
+    }
+
     // Parameters: layer name (or index) from Tiled, tileset, x, y
     const belowLayer = map.createLayer("Below Player", tileset, 0, 0);
-    const worldLayer = map.createLayer("World", tileset, 0, 0);
+    this.worldLayer = map.createLayer("World", tileset, 0, 0);
     const aboveLayer = map.createLayer("Above Player", tileset, 0, 0);
 
-    worldLayer.setCollisionByProperty({ collides: true });
+    if (!this.worldLayer || !aboveLayer) {
+      throw new Error("Failed to create map layers");
+    }
+
+    this.worldLayer.setCollisionByProperty({ collides: true });
 
     // By default, everything gets depth sorted on the screen in the order we created things. Here, we
     // want the "Above Player" layer to sit on top of the player, so we explicitly give it a depth.
@@ -33,74 +42,25 @@ export class Game extends Scene {
       (obj) => obj.name === "Spawn Point",
     );
 
-    // Create a sprite with physics enabled via the physics system. The image used for the sprite has
-    // a bit of whitespace, so I'm using setSize & setOffset to control the size of the player's body.
-    this.player = this.physics.add
-      .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
-      .setSize(30, 40)
-      .setOffset(0, 24);
+    if (!spawnPoint || typeof spawnPoint.x !== 'number' || typeof spawnPoint.y !== 'number') {
+      throw new Error("Failed to find spawn point");
+    }
+
+    // Create the player
+    this.player = new Player(this, spawnPoint.x, spawnPoint.y);
 
     // Watch the player and worldLayer for collisions, for the duration of the scene:
-    this.physics.add.collider(this.player, worldLayer);
+    this.physics.add.collider(this.player.getSprite(), this.worldLayer);
 
-    // Create the player's walking animations from the texture atlas. These are stored in the global
-    // animation manager so any sprite can access them.
-    const anims = this.anims;
-    anims.create({
-      key: "misa-left-walk",
-      frames: anims.generateFrameNames("atlas", {
-        prefix: "misa-left-walk.",
-        start: 0,
-        end: 3,
-        zeroPad: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    anims.create({
-      key: "misa-right-walk",
-      frames: anims.generateFrameNames("atlas", {
-        prefix: "misa-right-walk.",
-        start: 0,
-        end: 3,
-        zeroPad: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    anims.create({
-      key: "misa-front-walk",
-      frames: anims.generateFrameNames("atlas", {
-        prefix: "misa-front-walk.",
-        start: 0,
-        end: 3,
-        zeroPad: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    anims.create({
-      key: "misa-back-walk",
-      frames: anims.generateFrameNames("atlas", {
-        prefix: "misa-back-walk.",
-        start: 0,
-        end: 3,
-        zeroPad: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
     const camera = this.cameras.main;
-    camera.startFollow(this.player);
+    camera.startFollow(this.player.getSprite());
     camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-    var cursors = this.input.keyboard.createCursorKeys();
 
     // Help text that has a "fixed" position on the screen
     this.add
       .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
         font: "18px monospace",
-        fill: "#000000",
+        color: "#000000",
         padding: { x: 20, y: 10 },
         backgroundColor: "#ffffff",
       })
@@ -108,64 +68,24 @@ export class Game extends Scene {
       .setDepth(30);
 
     // Debug graphics
-    this.input.keyboard.once("keydown-D", (event) => {
-      // Turn on physics debugging to show player's hitbox
-      this.physics.world.createDebugGraphic();
+    const keyboard = this.input.keyboard;
+    if (keyboard) {
+      keyboard.once("keydown-D", () => {
+        // Turn on physics debugging to show player's hitbox
+        this.physics.world.createDebugGraphic();
 
-      // Create worldLayer collision graphic above the player, but below the help text
-      const graphics = this.add.graphics().setAlpha(0.75).setDepth(20);
-      worldLayer.renderDebug(graphics, {
-        tileColor: null, // Color of non-colliding tiles
-        collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-        faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
+        // Create worldLayer collision graphic above the player, but below the help text
+        const graphics = this.add.graphics().setAlpha(0.75).setDepth(20);
+        this.worldLayer?.renderDebug(graphics, {
+          tileColor: null, // Color of non-colliding tiles
+          collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+          faceColor: new Phaser.Display.Color(40, 39, 37, 255), // Color of colliding face edges
+        });
       });
-    });
+    }
   }
-  update(time, delta) {
-    const speed = 175;
-    const prevVelocity = this.player.body.velocity.clone();
 
-    // Stop any previous movement from the last frame
-    this.player.body.setVelocity(0);
-
-    var cursors = this.input.keyboard.createCursorKeys();
-
-    // Horizontal movement
-    if (cursors.left.isDown) {
-      this.player.body.setVelocityX(-speed);
-    } else if (cursors.right.isDown) {
-      this.player.body.setVelocityX(speed);
-    }
-
-    // Vertical movement
-    if (cursors.up.isDown) {
-      this.player.body.setVelocityY(-speed);
-    } else if (cursors.down.isDown) {
-      this.player.body.setVelocityY(speed);
-    }
-
-    // Normalize and scale the velocity so that player can't move faster along a diagonal
-    this.player.body.velocity.normalize().scale(speed);
-
-    // Update the animation last and give left/right animations precedence over up/down animations
-    if (cursors.left.isDown) {
-      this.player.anims.play("misa-left-walk", true);
-    } else if (cursors.right.isDown) {
-      this.player.anims.play("misa-right-walk", true);
-    } else if (cursors.up.isDown) {
-      this.player.anims.play("misa-back-walk", true);
-    } else if (cursors.down.isDown) {
-      this.player.anims.play("misa-front-walk", true);
-    } else {
-      this.player.anims.stop();
-
-      // If we were moving, pick and idle frame to use
-      if (prevVelocity.x < 0) this.player.setTexture("atlas", "misa-left");
-      else if (prevVelocity.x > 0)
-        this.player.setTexture("atlas", "misa-right");
-      else if (prevVelocity.y < 0) this.player.setTexture("atlas", "misa-back");
-      else if (prevVelocity.y > 0)
-        this.player.setTexture("atlas", "misa-front");
-    }
+  update(_time: number, _delta: number) {
+    this.player.update();
   }
 }
